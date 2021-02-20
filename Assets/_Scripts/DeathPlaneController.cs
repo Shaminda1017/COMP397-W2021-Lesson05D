658d05e6abea864cde8deb4a53fb8fde8d9e9977
Shaminda <shaminda1017@gmail.com>
2021-02-19 21:03:47 -0500

    created week 5

---
+++ Assets/_Scripts/DeathPlaneController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlaneController : MonoBehaviour
+{
+    public Transform spawnPoint;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // check if the object that trigger a collision is the player
+        if(other.gameObject.CompareTag("Player"))
+        {
+            // get a reference to the payer's CharectorController
+            var controller = other.gameObject.GetComponent<CharacterController>();
+            // turn controller off
+            controller.enabled = false;
+            // move the player to the spawnpoint
+            other.gameObject.transform.position = spawnPoint.position;
+            // turn controller on
+            controller.enabled = true;
+        }
+    }
+}
