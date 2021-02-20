35d358a0de36fea668793ed10775a04fae5f078b
Shaminda <shaminda1017@gmail.com>
2021-02-20 00:59:54 -0500

    added health bar lable

--- Assets/_Scripts/HealthBarWorldSpaceController.cs
+++ Assets/_Scripts/HealthBarWorldSpaceController.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class HealthBarWorldSpaceController : MonoBehaviour
 {
     public Transform playerCamera;
-   
+
+    void Start()
+    {
+        playerCamera = GameObject.Find("Player").transform;
+        playerCamera = GameObject.Find("PlayerCamera").transform;  
+    }
+
+
+
+
     // Update is called once per frame
     void LateUpdate()
     {
